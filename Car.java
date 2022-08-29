bb151343a2ef10fccc3cb3e4853f1fee1037fefe
Mahmut Can Gönül <75094927+MahmutCanGonul@users.noreply.github.com>
2022-08-29 15:37:35 +0300

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,9 @@
+package AkbankHGSProject;
+public class Car {
+    public long hgsNo;
+    public String name;
+    public String username;
+    public String type;
+    public String passedTime;
+    public int balance;
+}
